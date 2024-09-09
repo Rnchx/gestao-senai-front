5a40a272daca20dd69770339a900304d8ccf08c0
João Pedro Cassan da Rocha <123501793+Rnchx@users.noreply.github.com>
2024-09-09 08:23:22 -0300

    first commit

---
+++ app/page.jsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <div>
+      <DashHeader nome={"Felipe 92 Dev"} email={"dev.felipesantos@gmail.com"} />
+      <div style={{ marginLeft: 100 }}>
+        <p>Página principal</p>
+      </div>
+    </div>
+  );
+}
