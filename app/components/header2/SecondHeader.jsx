a2409795d6ea184d3dacd35cb20a33fa4b50abc2
Felipe Miotto <felipe.miotto@aluno.senai.br>
2024-10-02 09:30:36 -0300

    page completion

--- app/components/header2/SecondHeader.jsx
+++ app/components/header2/SecondHeader.jsx
@@ -3,7 +3,7 @@ import style from './secondHeader.module.css';
 const SecondHeader = () => {
   return (
       <div className={style.headerContainer}>
-        <div className={style.divImg}>
+        
         <img src="Logo.png" alt="Logo da Empresa" height={150} width={300} className={style.logo}/>
         <img src="Logo.png" alt="Logo da Empresa" height={150} width={300} className={style.logo}/>
         <img src="Logo.png" alt="Logo da Empresa" height={150} width={300} className={style.logo}/>
@@ -11,7 +11,7 @@ const SecondHeader = () => {
         <img src="Logo.png" alt="Logo da Empresa" height={150} width={300} className={style.logo}/>
         <img src="Logo.png" alt="Logo da Empresa" height={150} width={300} className={style.logo}/>
         <img src="Logo.png" alt="Logo da Empresa" height={150} width={300} className={style.logo}/>
-        </div>
+       
       </div>
   )
 }
