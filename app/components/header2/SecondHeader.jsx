3ff29ac43bdeaa5c9e80ce7f219d7f78ee69410f
Felipe Miotto <felipe.miotto@aluno.senai.br>
2024-10-02 09:17:07 -0300

    att page Header2 and Logo.png

--- app/components/header2/SecondHeader.jsx
+++ app/components/header2/SecondHeader.jsx
@@ -2,9 +2,15 @@ import style from './secondHeader.module.css';
 
 const SecondHeader = () => {
   return (
-      <div className='headerContainer'>
-        <div className='divImg'>
-        <img src="SENAIlogo.png" alt="Logo da Empresa" height={100} width={300} class="logo"/>
+      <div className={style.headerContainer}>
+        <div className={style.divImg}>
+        <img src="Logo.png" alt="Logo da Empresa" height={150} width={300} className={style.logo}/>
+        <img src="Logo.png" alt="Logo da Empresa" height={150} width={300} className={style.logo}/>
+        <img src="Logo.png" alt="Logo da Empresa" height={150} width={300} className={style.logo}/>
+        <img src="Logo.png" alt="Logo da Empresa" height={150} width={300} className={style.logo}/>
+        <img src="Logo.png" alt="Logo da Empresa" height={150} width={300} className={style.logo}/>
+        <img src="Logo.png" alt="Logo da Empresa" height={150} width={300} className={style.logo}/>
+        <img src="Logo.png" alt="Logo da Empresa" height={150} width={300} className={style.logo}/>
         </div>
       </div>
   )
