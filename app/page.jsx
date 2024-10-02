903ed16e0c0f9ebb957af2984c1745bc68327620
Enrico Lona <enrico-lona@aluno.senai.br>
2024-10-02 17:17:26 -0300

    Refactoriza o componente Header e estilos
    Refatora o componente Header e os estilos relacionados para melhorar a estrutura e o layout da página inicial.

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import style from './page.module.css';
-import Header from "./components/header/Header";
+import Header from "./components/header/Header";  
 
 export default function Home() {
   return (
@@ -9,15 +9,18 @@ export default function Home() {
       <div className={style.page}>
         <div className={style.overlay}>
           <div className={style.loginBox}>
-            <h2>Login</h2>
-            <form>
-              <label htmlFor="cpf">CPF</label>
-              <input type="text" id="cpf" name="cpf" required />
-              <label htmlFor="password">Senha</label>
-              <input type="password" id="password" name="password" required />
-              <button type="submit">Entrar</button>
-            </form>
+            <h2 className={style.h2}>Login</h2>
+            <div className={style.inputBox}>
+              <form>
+                <label htmlFor="cpf">CPF</label>
+                <input type="text" id="cpf" name="cpf" required />
+                <label htmlFor="password">Senha</label>
+                <input type="password" id="password" name="password" required />
+              </form>
+            </div>
+            
           </div>
+          <button className={style.button} type="submit">Entrar</button>
         </div>
       </div>
     </div>
