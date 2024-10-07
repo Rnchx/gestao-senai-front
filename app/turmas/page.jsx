484b6e7f0cc8d377a61fe6ec1b92c7d9c7569646
analiviamartins <anamartins@aluno.senai.br>
2024-10-07 11:40:27 -0300

    feat: Navegação entre salas

---
+++ app/turmas/page.jsx
@@ -0,0 +1,55 @@
+import Link from "next/link";
+import ButtonClasses from "../components/buttonClasses/ButtonClasses";
+
+const Navigation = () => {
+    const handleNavigation = (path) => {
+        window.location.href = path
+    }
+ }
+
+return (
+    <>
+        <div>
+            <button type="button" onClick={ }>Voltar</button>
+        </div>
+        <div>
+            <Text>Itinerário formativo</Text>
+        </div>
+        <div>
+            <ButtonClasses
+                text={"2TD1"}
+                onClick={() => handleNavigation('/page1')}
+            />
+            <ButtonClasses
+                text={"2TDS2"}
+                onClick={() => handleNavigation('/page2')}
+            />
+            <ButtonClasses
+                text={"2TEE"}
+                onClick={() => handleNavigation('/page3')}
+            />
+            <ButtonClasses
+                text={"2TFM"}
+                onClick={() => handleNavigation('/page4')}
+            />
+            <ButtonClasses
+                text={"4TDS1"}
+                onClick={() => handleNavigation('/page5')}
+            />
+            <ButtonClasses
+                text={"4TDS2"}
+                onClick={() => handleNavigation('/page6')}
+            />
+            <ButtonClasses
+                text={"4TEE"}
+                onClick={() => handleNavigation('/page7')}
+            />
+            <ButtonClasses
+                text={"4TFM"}
+                onClick={() => handleNavigation('/page8')}
+            />
+        </div>
+    </>
+);
+
+export default Navigation;
