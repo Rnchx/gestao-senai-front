7c675722c6437a654a21a3a2526e56c0314bdbb5
fe miotto <felipe.miotto@aluno.senai.br>
2024-10-09 16:35:03 -0300

    page inicio

--- app/inicio/page.jsx
+++ app/inicio/page.jsx
@@ -1,19 +1,23 @@
 import style from './inicio.module.css';
 import SecondHeader from '../components/header2/SecondHeader';
+import Image from 'next/image';
 
 export default function Home() {
     return (
         <div>
             <SecondHeader />
             <div className={style.page}>
-                <div className={style.overlay}>
-                   <div className={style.container}>
-                    <h1 className={style.text}>Registro de alunos</h1>
-                   </div>
 
-                   <div className={style.container}>
-                    <h1 className={style.text}>AAPM</h1>
-                   </div>
+                <div className={style.overlay}>
+                    <div className={style.container}>
+                        <h1 className={style.text}>Registro de alunos</h1>
+                        <img className={style.img} src="alunos.png" alt="icone" />
+                    </div>
+
+                    <div className={style.container}>
+                        <h1 className={style.text}>AAPM</h1>
+                        <img className={style.img} src="AAPM.png" alt="icone" />
+                    </div>
                 </div>
             </div>
         </div>
