6e699837ec42342602a3062c9bf8a503da79fb33
analiviamartins <analiviamartins@aluno.senai.br>
2024-10-23 16:41:19 -0300

    comecei o modal

--- app/registroAlunos/page.jsx
+++ app/registroAlunos/page.jsx
@@ -1,29 +1,10 @@
-import style from './registroAlunos.module.css';
-import SecondHeader from '../components/header2/SecondHeader';
+import style from './armarios.module.css';
 
-export default function Home() {
+
+export default function Armarios() {
     return (
         <div>
-            <SecondHeader />
-            <div className={style.page}>
-
-                <div className={style.overlay}>
-                    <div className={style.container}>
-                        <h1 className={style.text}>Itinerário formativo</h1>
-                        <img className={style.img} src="caca.png" alt="icone" />
-                    </div>
-
-                    <div className={style.container}>
-                        <h1 className={style.text}>...</h1>
-                        <img className={style.img} src="" alt="icone" />
-                    </div>
-
-                    <div className={style.container}>
-                        <h1 className={style.text}>Industrial</h1>
-                        <img className={style.img} src="solucao.png" alt="icone" />
-                    </div>
-                </div>
-            </div>
+            
         </div>
     );
 }
