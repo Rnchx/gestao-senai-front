0dc9ba3b875daec1f60fcc7ca1883b32253731b1
João Pedro Cassan da Rocha <123501793+Rnchx@users.noreply.github.com>
2024-10-30 17:17:04 -0300

    feat: start curses page

--- app/eachTypeOfCurse/[curse]/page.jsx
+++ app/eachTypeOfCurse/[curse]/page.jsx
@@ -2,48 +2,79 @@
 
 import style from './curse.module.css';
 import SecondHeader from '../../components/header2/SecondHeader';
+import axios from 'axios';
 import { useEffect, useState } from 'react';
-import { useRouter } from 'next/navigation';
+import { useSearchParams } from 'next/navigation';
+
+import imagem from "../../../public/AAPM.png"
 
 export default function EachCurse() {
-
   const [students, setStudents] = useState([]);
+  const [error, setError] = useState(null);
 
-  // const apiUrlgetAllStudents = process.env.apiUrlGetStudents;
-  const apiUrlgetAllStudents = "localhost:4000/students"
+  const searchParams = useSearchParams();
+  const curso = searchParams.get('curso');
 
-  const fetchStudents = async () => {
+  const apiUrls = {
+    'Técnico': process.env.NEXT_PUBLIC_API_GET_STUDENTS_TECNICO,
+    'Itinerário Formativo': process.env.NEXT_PUBLIC_API_GET_STUDENTS_FORMATIVO,
+    'Industrial': process.env.NEXT_PUBLIC_API_GET_STUDENTS_INDUSTRIAL,
+  };
+
+  const fetchStudents = async (url) => {
     try {
-      const response = await axios.get(apiUrlgetAllStudents);
-      setStudents(response.data.students);
+      const response = await axios.get(url);
+      setStudents(response.data.students || response.data);
     } catch (error) {
       console.error(error);
+      setError('Erro ao carregar os dados dos estudantes');
     }
   };
 
   useEffect(() => {
-    fetchStudents();
-  }, []);
+    const apiUrl = apiUrls[curso];
+    if (apiUrl) {
+      fetchStudents(apiUrl);
+    } else {
+      setError('Curso inválido');
+    }
+  }, [curso]);
 
   return (
     <div>
       <SecondHeader />
-      <div className={style.page}>
-        <h1>{  }</h1>
-        <div className={style.alunoContainer}>
-          {students.map((student) => (
-            <div key={student.id} className={style.alunoCard}>
-              <p>Nome: {student.name}</p>
-              <p>Data de nascimento: {student.dateOfBirth}</p>
-              <p>Classe: {student.studentClass}</p>
-              <p>Tipo de curso: {student.courseType}</p>
-              <img src={student.carometer} alt="foto do aluno" />
-              <p>Participante da AAPM: {student.aapmStatus}</p>
-              <p>Disponível para estágio: {student.internshipStatus}</p>
-            </div>
-          ))}
-        </div>
+      <img src={imagem} alt="foto teste" />
+      <div className={style.containerFilters}>
+        <h1 className={style.titleCourse}> Alunos - {curso}</h1>
+
+        <button className={style.buttonsFilter}>18 anos</button>
+        <button className={style.buttonsFilter}>Aprendiz</button>  
+        <button className={style.buttonsFilter}>Estagiário</button>
+          <button className={style.buttonsFilter}>Contribuentes AAPM</button>
+
       </div>
-    </div>
+      <div className={style.containerCards}>
+        {error && <p>{error}</p>}
+        <div className={style.alunoContainer}>
+          {students.length > 0 ? (
+            students.map((student) => (
+              <div key={student.id} className={style.alunoCard}>
+                <img className={style.imageStudent} src={student.carometer} alt="foto do aluno" />
+                <p className={style.nameStudent}><b>{student.name}</b></p>
+                {/* <p>{student.dateofbirth}</p>
+                <p>{student.studentclass}</p>
+                <p>{student.coursetype}</p>
+                <p>Participante da AAPM: {student.aapmstatus ? "Sim" : "Não"}</p>
+                {console.log(student.aapmstatus)}
+                <p>Disponível para estágio: {student.internshipstatus ? "Sim" : "Não"}</p>
+                {console.log(student.internshipstatus)} */}
+              </div>
+            ))
+          ) : (
+            <p>Carregando estudantes...</p>
+          )}
+        </div>
+          </div>
+      </div>
   );
 }
--- app/tipoCursos/page.jsx
+++ app/tipoCursos/page.jsx
@@ -25,21 +25,21 @@ export default function TipoCursos() {
             <SecondHeader />
             <div className={style.page}>
                 <div className={style.overlay}>
-                    <a className={style.linkImage} href="/eachTypeOfCurse/itinerario-formativo">
+                    <a className={style.linkImage} href="/eachTypeOfCurse/itinerario-formativo?curso=Itinerário Formativo">
                         <div className={style.container}>
                             <h1 className={style.text}>Itinerário Formativo</h1>
                             <img className={style.img} src="caca.png" alt="icone" />
                         </div>
                     </a>
 
-                    <a className={style.linkImage} href="/eachTypeOfCurse/tecnico">
+                    <a className={style.linkImage} href="/eachTypeOfCurse/tecnico?curso=Técnico">
                         <div className={style.container}>
                             <h1 className={style.text}>Técnico</h1>
                             <img className={style.img} src="iconeTecnico.png" alt="icone" />
                         </div>
                     </a>
 
-                    <a className={style.linkImage} href="/eachTypeOfCurse/industrial">
+                    <a className={style.linkImage} href="/eachTypeOfCurse/industrial?curso=Industrial">
                         <div className={style.container}>
                             <h1 className={style.text}>Industrial</h1>
                             <img className={style.img} src="solucao.png" alt="icone" />
