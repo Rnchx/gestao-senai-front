87e769bdbfce5188761f6bdcb6accb936a1f9ccd
analiviamartins <anamartins@aluno.senai.br>
2024-11-13 10:48:44 -0300

    adjusts page login

--- app/components/login/Login.jsx
+++ app/components/login/Login.jsx
@@ -2,44 +2,43 @@
 
 import { useState } from "react";
 import { useAuth } from '../authContext/user';
+import axios from 'axios';
 
 export const useLogin = () => {
-    const { login } = useAuth(); // Usar a função de login do contexto
+    const { login } = useAuth();
     const [error, setError] = useState(null);
     const [isLoading, setIsLoading] = useState(false);
 
     const logIn = async (cpf, password) => {
         const api = process.env.NEXT_PUBLIC_LOGIN
-
+    
         setError(null);
-        setIsLoading(true);
+        setIsLoading(true);   
+
+        // console.log(api, "link para fazer o login do back");
+        // console.log(cpf, "cpf do adiministrador");
+        // console.log(password, "senha do adiministrador");
 
         try {
-            const response = await fetch(api, {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({ cpf, password }),
+            const response = await axios.post(api, { cpf, password }, {
+              
+              headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+              },
+              credentials: "include",
             });
-
-            if (!response.ok) {
-                throw new Error("Falha na autenticação");
-            }
-
-            const data = await response.json();
-            const token = data.token;
-
+        
+            const { token } = response.data;
             login(token);
-
             return true;
-        } catch (err) {
+          } catch (err) {
             setError(err.message);
             return false;
-        } finally {
+          } finally {
             setIsLoading(false);
-        }
-    };
+          }
+        };
 
     return { logIn, error, isLoading };
 };
