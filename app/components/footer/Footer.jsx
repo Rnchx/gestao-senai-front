f13b3eece8ba13f2b58c69ae78d8c6243ae16e89
Sophia Marcondes <sophiamarcondes@aluno.senai.br>
2024-12-02 11:49:15 -0300

    fix3

--- app/components/footer/Footer.jsx
+++ app/components/footer/Footer.jsx
@@ -3,7 +3,20 @@ import style from "./footer.module.css";
 const Footer = () => {
     return (
         <div className={style.footerContainer}>
+            <div className={style.texto}>
             <p>©Todos os direitos reservados</p>
+            </div>
+            
+        <div className={style.imagem}>
+        <img
+          src="Logo.png"
+          alt="Logo da Empresa"
+          className={style.logo}
+        />
+        </div>
+            
+    
+
         </div>
     );
 };
